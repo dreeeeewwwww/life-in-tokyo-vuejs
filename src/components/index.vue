9465c7cf1a738ba9cc085dc8af288f4f499dc7d9
Admin <andrew.david.gardner@gmail.com>
2018-05-05 21:15:22 -0400

    add bootstrap cdn and font awesome cdn to app css import

--- src/components/index.vue
+++ src/components/index.vue
@@ -1,7 +1,12 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p><strong>this is testing scss</strong></p>
+  <div class="container">
+    <div class="row">
+      <div class="col-6 hello">
+        <h1>{{ msg }}</h1>
+        <p><strong>this is testing scss</strong></p>
+        <p><i class="fas fa-allergies"></i></p>
+      </div>
+    </div>
   </div>
 </template>
 
