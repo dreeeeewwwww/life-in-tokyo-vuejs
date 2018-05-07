fc118e45aa33a2fc5eedddab4d1cfb33f706a8c8
Admin <andrew.david.gardner@gmail.com>
2018-05-06 22:04:29 -0400

    add archive data view

--- src/components/archive.vue
+++ src/components/archive.vue
@@ -1,19 +1,31 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p><strong>{{ foo }}</strong></p>
+  <div>
+
+    <h1 class="mb-3 pb-3">{{ msg }}</h1>
+    <div v-for="archive in data.archives" :key="archive.id" class="row mb-3 pb-3">
+      <div class="col-3">
+        <h2>{{ archive.name }}</h2>
+      </div>
+      <div class="col-9">
+        <div class="row">
+          <div v-for="content in archive.contents" :key="content.id" class="col-3">
+            <img :src="content.img_thumb" class="img-fluid mb-3"/>
+            {{ content.title }}
+          </div>
+        </div>
+      </div>
+    </div>
+
   </div>
 </template>
 
 <script>
-import archives from '../assets/data/archives.json'
+import data from '../assets/data/archives.json'
 export default {
-  name: 'archive',
   data () {
     return {
-      msg: 'this is the archive page',
-      foo: 'some other output',
-      archives
+      msg: 'Archive',
+      data
     }
   }
 }
