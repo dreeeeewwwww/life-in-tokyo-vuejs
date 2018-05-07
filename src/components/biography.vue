7ffc4a7cd4cc357f21fb59770f34003d25bb35c6
Admin <andrew.david.gardner@gmail.com>
2018-05-06 20:43:49 -0400

    add placeholder bio content, remove conflicting tab characters from text

--- src/components/biography.vue
+++ src/components/biography.vue
@@ -1,7 +1,143 @@
 <template>
-  <div class=" hello">
-    <h1>{{ msg }}</h1>
-    <p><strong>{{ foo }}</strong></p>
+  <div class="">
+
+    <section id="children" class="mb-3 pb-3">
+      <div class="row">
+        <div class="col-md-6 offset-md-3">
+          <img class="img-fluid d-block" src="../assets/images/biography_glam.jpg" />
+          <cite>Japan in 1978. Mick Karn, Rob Dean, David Sylvian, Richard Barbieri and Steve Jansen.</cite>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-3">
+          <h1>CHILDREN OF GLAM</h1>
+          <h2>1974 - 1978</h2>
+        </div>
+        <div class="col-6">
+          <p>
+            Looking and sounding a bit like The New York Doll"s snotty, misguided younger brothers, the generally
+            derivative glam leanings of British band Japan"s formative years gives one little indication of the critical and commercial success the group would achieve by the time of their disbandment in 1982.
+          </p>
+          <p>
+            Formed in London in 1974 as an after school diversion for brothers David and Steve Batt (later Sylvian and Jansen, respectively) and friend Anthony Michelides (later Mick Karn), the group began practicing at the Batt"s home <span class="lilreference">1</span>. Looking to flesh out their sound, the trio enlisted classmate Richard Barbieri to play keyboards and found lead guitarist Rob Dean through an ad posted in Melody Maker <span class="lilreference">2</span>. The name Japan was supposedly decided on backstage before an early gig, when the band realized at the last minute that they needed to call themselves something <span class="lilreference">2</span>.
+          </p>
+          <p>
+            Sylvian"s calculated androgynous posturing gained the attention of manager Simon Napier-Bell (industry veteran behind The Yardbirds, Dusty Springfield, Marc Bolan and later Wham!), who said Sylvian resembled "sixties Jagger crossed with teenage Bardot; young Elvis with adolescent Fonda" <span class="lilreference">3</span>. Napier-Bell took on the band, though he originally only wanted to manage Sylvian as a solo artist <span class="lilreference">2</span>. Following a abel sponsored talent contest in 1977, which Japan lost to The Cure, the group signed to German label Ariola-Hansa and began modest touring, including opening for The Damned during a UK tour that spring <span class="lilreference">4</span>.
+          </p>
+          <p>
+            Their first two albums, "Adolescent Sex" and "Obscure Alternatives," released by Ariola-Hansa in 1978, yielded little success in the UK, though the group did catch on in Japan (likely based on the name and their pin-up looks). Mostly filled with routine rock numbers covered in a trashy glam-escent coating, the first two albums did manage a handful of interesting tracks, including a few with definite funk, soul and reggae influences. During this time the band toured with Blue &Ouml;yster Cult, however, their opening set was generally met with mixed reactions. Their big hair and snarly sound were clearly not working out, both personally and commercially, and by 1979 Japan began heading in a completely different direction.
+          </p>
+        </div>
+      </div>
+    </section>
+
+    <section id="quiet">
+      <img class="img-fluid d-block" src="../assets/images/biography_moroder.jpg" />
+      <cite>In the studio with Moroder, Los Angeles 1979.</cite>
+      <h1>A QUIET LIFE IN TOKYO</h1>
+      <h2>1979 - 1980</h2>
+      <div class="row">
+        <div class="col-md-8 offset-md-1">
+          <p>
+            Aligning themselves with disco czar Giorgio Moroder, the 1979 single "Life in Tokyo" marked a major turning point for Japan. Already having written several new songs in Moroder"s style, including the later released "European Son," the caterpillar-lipped producer seemed a likely choice for the band. Now singing with a bit of a shouty croon, the "Life in Tokyo" Sylvian positioned himself as a sort of Bryan Ferry for the coming decade, complete with clipped coif and red leather blazer. Backed by a precision beat from Jansen, wiggly bass
+            and spooky woodwinds from Karn, icy synthesizers from Barbieri, and atmospheric guitar from Dean, the single laid a good deal of the groundwork for what the band would create over their next three albums.
+          </p>
+          <p>
+            Their last album for Ariola-Hansa, 1979"s "Quiet Life," carried the feel over from "Life in Tokyo," particularly on the title track. Showing an exponential increase in artistic control and production quality, the opening track's fey disco gives way to an album of moody dance rock and dark atmospherics. The album marks Japan's first collaboration with producer John Punter, who had, among other projects, engineered Roxy Music's first three albums and produced their 1974 LP, "Country Life." Japan later hailed Quiet Life" as a major turning point. Barbieri said in a 1982 interview, "when we recorded it we felt it was the best thing we'd ever done. I'll always be happy with this album." In the same interview Sylvian said, "I still feel very attached to it - unusual for me. We reached a peak with this album - we knew what we were doing" <span class="lilreference">5</span>. Despite all this, the album, peaking at 53 on UK charts, again didn"t manage to do particularly well <span class="lilreference">6</span>.
+          </p>
+          <p>
+            Still looking to break the singles charts, the band, under Napier-Bell"s suggestion, planned to release a cover version. They chose Smokey Robinson"s "I Second That Emotion," and approached it from a generally reverent angle. Sylvian continued to tweak his image towards "the 'Quentin Crisp' look, sleek in well-cut suits, waistcoats, bow-ties and immaculate bouffant courtesy of Mane Line" <span class=
+            "lilreference">7</span>. The rest of the band opted to not go the way of the <a href="http://www.discogs.com/release/98735">Great Dark Man</a>, though they otherwise continued to posh up their looks. They also cooked up [connie citiation reynolds] publicity ideas, including dubbing Sylvian as "The Most Beautiful Man in the World" <span class="lilreference">2</span>. Beautiful or not, "I Second that Emotion"
+            failed to break through. Karn said of the single in 1982, "I"ve always hated that song...not the song but our rendition of it. It's got such a gloomy, cotton-wool-wrapped-round-it atmosphere...it"s got nothing to do with us now at all" <span class="lilreference">8</span>. Ariola-Hansa opted not to keep the band for further releases, prompting Japan to move to Virgin Records.
+          </p>
+        </div>
+      </div>
+    </section>
+
+    <section id="new-romance">
+      <img class="img-fluid mx-auto d-block" src="../assets/images/biography_backstage.jpg" /><br />Sylvian, Karn, Barbieri, Jansen and guitarist Masami Tsuchyia backstage during the "Sons of Pioneers" tour, 1982.
+      <h1>KINGS OF NEW ROMANCE</h1><br /><h2>1980 - 1982</h2>
+        <p>
+          Picking up musically where "Quiet Life," and visually where "I Second that Emotion" left off, their next album, 1980"s "Gentlemen Take Polaroids," marked another exponential leap in style and substance. Their first of two albums on Virgin Records, "Gentlemen Take Polaroids" perfectly pairs Sylvian"s svelte crooning of his peculiar, yet nonetheless evocative, lyrics with the increasingly unique musical voices of his band mates.
+          The LP is notable as it includes the first of many collaborations between Sylvian and Japanese music
+          icon Ryuichi Sakamoto (on the track, "Taking Islands in Africa") and also because it is the last album recorded with guitarist Rob Dean.
+          </p>
+          <p>
+          Dean's departure from the group, prior to the "Visions of China" tour in 1981,  stemmed from the band"s increasing emphasis on synthesizers. In a 1986 interview Dean said, "...I'd really left before I left...I wasn't that much involved and it was my fault...I guess we all knew there was going to be a change" <span class="lilreference">9</span>. Ironically, however, the group enlisted David Rhodes to play guitar on tour in 1981, and later recruited Ippu-Do front man Masami Tsuchyia to play guitar on their final tour in 1982.
+          </p>
+          <p>
+          November 1981 saw the release of Japan"s final album, "Tin Drum." Widely touted as the band"s masterpiece, the LP peaked at 12 on the UK charts and produced the unlikely top 5 single, "Ghosts." With a brooding vocal, sparse synthesizers and even sparser percussion, it's a wonder that they got Virgin to even consider the track as a single. Sylvian said, "Writing 'Ghosts' was a turning point for me...I'd felt I'd had the breakthrough I was looking for. I'd touched upon something true to myself and expressed it in a way that didn't leave me
+          feeling overly vulnerable" <span class="lilreference">10</span>. The rest of the songs, from the jerky white funk of "The Art of Parties," to the epic beat of "Visions of China," make "Tin Drum" stand as one of the most completely solid albums of the 1980s. Though, with Mao on the cover and song titles like, "Canton," "Visions of China," and, "Cantonese Boy," it does make one wonder why a band called Japan is so into China.
+        </p>
+        <p>
+          Riding the wave of "Tin Drum"s" sudden success, the band embarked on two major tours, "Visions of China" in 1981 and "Sons of Pioneers" in 1982. The latter  produced the concert film "Oil on Canvas," filmed at London"s Hammersmith Odeon, as well as a live double LP of the same name. Around this time Ariola-Hansa, much to the band's dismay, leapt on the opportunity to re-issue Japan's back catalog. Years after their original release, tracks like "I Second That Emotion," "Quiet Life," and "European Son" (which was previously only the B-side of singles), managed to hit the charts. It must have been quite spectacular for newer fans, as record shelves were flooded with five albums worth of "new" material.
+        </p>
+        <p>
+          Despite their successful record and tours, the band, after much press speculation, decided to call it quits after the "Sons of Pioneers" tour. Long-running differences between Karn and Sylvian are cited as the cause for the break up, which likely came to a head when Karn"s girlfriend, Yuka Fujii, left him for Sylvian in '81 <span class="lilreference">11</span>. Fujii had previously been a frequent collaborator with Japan, providing artwork for record sleeves and even singing backing vocals on, "Tin Drum." Under the advisement of the record company, Japan held things together up until their final release, the live album, "Oil on Canvas," before finally parting ways.
+        </p>
+    </section>
+
+    <section id="solo">
+      <h1>Solo years</h1><br /><h2>1983 -1990</h2>
+        <p>
+          1982 saw the release of Karn"s first solo album, "Titles." In 1984 Sylvian made his solo debut with the LP "Brilliant Trees." Jansen and Barbieri, under the name The Dolphin Brothers, collaborated on the 1987 album "Catch the Fall." Rob Dean went on to work with the bands Vivabeat and Illustrated Man, and additionally played on Gary Numan's 1981 album, "Dance" (which Karn also guested on), as well as on Sin&eacute;ad O'Conner's 1987 debut, "The Lion and the Cobra." Dean has since moved to Costa Rica and is an avid bird watcher <span class="lilreference">12</span>.</p>
+    </section>
+
+    <section id="rain-tree-crow">
+      <h1>Rain Tree Crow</h1><br /><h2>1983 -1990</h2>
+        <p>
+          In 1989-1990 Sylvian, Jansen, Karn and Barbieri reunited under the name Rain Tree Crow to record a self-titled album. After its release in 1991 the group once again disbanded. Jansen, Barbieri and Karn reunited again in the 1990s under the moniker JBK and released a pair of albums on their label Medium Productions. Karn has continued to release solo albums, while Jansen has continued to collaborate with a variety of musicians, including working on Annie Lennox's "Diva." Barbieri has played with the band Porcupine Tree since the early '90s and released his first solo album, "Things Buried," in 2005. Sylvian has since married, and divorced, former Prince prot"g" Ingrid Chavez, and, among other projects, started his own label, Samadhisound.
+        </p>
+    </section>
+
+    <section id="today">
+      <h1>Today or something</h1><br /><h2>1983 -1990</h2>
+      <p>
+        In 1989-1990 Sylvian, Jansen, Karn and Barbieri reunited under the name Rain Tree Crow to record a self-titled album. After its release in 1991 the group once again disbanded. Jansen, Barbieri and Karn reunited again in the 1990s under the moniker JBK and released a pair of albums on their label Medium Productions. Karn has continued to release solo albums, while Jansen has continued to collaborate with a variety of musicians, including working on Annie Lennox's "Diva." Barbieri has played with the band Porcupine Tree since the early '90s and released his first solo album, "Things Buried," in 2005. Sylvian has since married, and divorced, former Prince prot"g" Ingrid Chavez, and, among other projects, started his own label, Samadhisound.
+      </p>
+    </section>
+
+      <p>&nbsp;</p>
+      <p>###</p>
+      <p>&nbsp;</p>
+
+    <section id="references">
+      <p>REFERENCES</p>
+      <ol>
+        <li>Hoshiko, Seiichi. "Childhood Memories of Steve Jansen." Viva Rock. 1985. Translated in Bamboo. Issue 8, 1986. p. 49.
+        </li>
+        <li>Rymer, Paul. "Biography." Japan Nightporter. <a href="http://www.nightporter.co.ukpages/historyo.htm">
+          http://www.nightporter.co.ukpages/historyo.htm</a></li>
+        <li>Napier-Bell, Simon. "Sign Here, Kid." Excerpt from Black Vinyl, White Powder. Ebury Press, London. 2001.
+          <a href="http://www.simonnapierbell.com/writingbooks_main.htm#Sign%20here,%20kid">
+          http://www.simonnapierbell.com/writingbooks_main.htm#Sign%20here,%20kid</a></li>
+        <li>Ankeny, Jason. "Japan Biography." All Music Guide. <a href="http://wc03.allmusic.com/cg/amg.dll?p=amg&sql=11:kifoxqe5ldae~T1">
+          http://wc03.allmusic.com/cg/amg.dll?p=amg&sql=11:kifoxqe5ldae~T1</a></li>
+        <li>"Five Records Re-Viewed." Fan Library - Japan. July, 1982. p. 26-27.</li>
+        <li>Rymer, Paul. "Quiet Life." Japan Nightporter. <a href="http://nightporter.co.uk/pages/quiet_album.htm">
+          http://nightporter.co.uk/pages/quiet_album.htm</a></li>
+        <li>"Ghost Image." Fan Library - Japan. July, 1982. p. 34-39.</li>
+        <li>Sunie (?). "Karn Buy Me Love." Record Mirror. July 24, 1982.</li>
+        <li>Sawyer, Howie & Zornes, Debi. "Rob Dean - A Day in the Life. Interview Pt. 1." Bamboo. Issue 9, 1986. p. 15.</li>
+        <li>Dahlen, Chris. "David Sylvian Biography " July 2007." Davidsylvian.com. <a href=
+          "http://www.davidsylvian.com/other_writings/david_sylvian_biography_july_2007.html">
+          http://www.davidsylvian.com/other_writings/david_sylvian_biography_july_2007.html</a></li>
+        <li>Hennessy, Louisa. "Mick Karn " Because You Demanded It, Louisa Hennessy Strips Mick Karn of His Defenses." ZigZag Magazine. April,
+          1982.</li>
+        <li><a href="http://a8creative.co.uk/orpheus/robdean.htm">http://a8creative.co.uk/orpheus/robdean.htm</a></li>
+      </ol>
+    </section>
+
+    <section id="resources">
+      <p>RESOURCES</p>
+      <ol>
+        <li><a href="">Mick Karn official site</a></li>
+        <li><a href="">Steve Jansen official site</a></li>
+        <li><a href="">Richard Barbieri official site</a></li>
+        <li><a href="">Samadhi Sound</a></li>
+      </ol>
+    </section>
+
   </div>
 </template>
 
