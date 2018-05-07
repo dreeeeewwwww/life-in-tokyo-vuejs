42c9f264594807fe878247a4ec726266ce8d611d
Admin <andrew.david.gardner@gmail.com>
2018-05-07 03:02:37 -0400

    add v-if for video and image views

--- src/components/images.vue
+++ src/components/images.vue
@@ -1,6 +1,21 @@
 <template>
   <div class=" hello">
     <h1>{{ msg }}</h1>
+
+    <div v-for="image in data.images" :key="image.id" class="row mb-3 pb-3">
+      <div class="col-3">
+        <h2>{{ image.name }}</h2>
+      </div>
+      <div v-if="image.name === 'band'" class="col-9">
+        <div class="row">
+          <div v-for="content in image.contents" :key="content.id" class="col-3">
+            <img :src="content.thumb" class="img-fluid mb-3"/>
+            {{ content.title }}
+          </div>
+        </div>
+      </div>
+    </div>
+
   </div>
 </template>
 
--- src/components/video.vue
+++ src/components/video.vue
@@ -1,6 +1,21 @@
 <template>
-  <div class=" hello">
+  <div>
     <h1>{{ msg }}</h1>
+
+    <div v-for="video in data.videos" :key="video.id" class="row mb-3 pb-3">
+      <div class="col-3">
+        <h2>{{ video.name }}</h2>
+      </div>
+      <div v-if="video.id" class="col-9">
+        <div class="row">
+          <div v-for="content in video.contents" :key="content.id" class="col-3">
+            <img :src="content.thumb" class="img-fluid mb-3"/>
+            {{ content.title }}
+          </div>
+        </div>
+      </div>
+    </div>
+
   </div>
 </template>
 
